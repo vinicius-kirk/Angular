14770d56089ff46719f363ac303e836ffe3f27a3
Vinicius Santos de A Teixeira <vinicius.teixeira@wscorp.com.br>
2021-05-12 17:12:57 -0300

    AA

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,60 @@
+import { listLazyRoutes } from "@angular/compiler/src/aot/lazy_routes";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+
+@Component({
+    selector:'pm-products',
+    templateUrl:'./product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit {
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
+    pageTitle = 'Product List';
+    imageWidth = 50;
+    imageMargin = 2;
+    showImage : boolean = false;
+
+    private _listFilter : string = '';
+    get listFilter() : string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter = value;
+        console.log('In setter: ', value)
+
+    }
+
+
+    products: IProduct[] = [
+        {
+            "productId":2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 15,2021",
+            "description": "15 gallon capacity rolling garden cart",
+            "price":32.99,
+            "starRating":4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId":5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048" ,
+            "releaseDate": "May 21,2021",
+            "description": "Curved claw steel hammer",
+            "price":8.9,
+            "starRating":4.8,
+            "imageUrl": "assets/images/hammer.png"
+        }
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    
+} 
+
